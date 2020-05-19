c89f2ab05cbdcac00df052b251bfd1a037768179
sahilthakur7 <sahilrd07@gmail.com>
2020-05-19 17:36:41 +0530

    Fixed one typo

--- comments/index.js
+++ comments/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 
 const app = express();
-app.use(bodyParse.json());
+app.use(bodyParser.json());
 
 const commentsByPostId = {};
 
