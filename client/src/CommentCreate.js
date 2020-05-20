50c25248c529a1b7ac9f9c1c27a90aadab3ea08d
sahilthakur7 <sahilrd07@gmail.com>
2020-05-20 15:24:09 +0530

    Listing and creating comments done

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (e) => {
+    if (e) {
+      e.preventDefault();
+    }
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            className="form-control"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <button className="btn btn-primary mt-2">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
